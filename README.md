ef49529702a4cb3a4ef50e93658716931fc6b3f7
rydengraham <ryden3.14@gmail.com>
2023-02-08 09:23:13 -0700

    Finished everything.

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ Contributors / Licensing
 Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle, 
 https://github.com/tywtyw2002, and https://github.com/treedust
 
+Modified 2023 by Ryden Graham
+
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
--- httpclient.py
+++ httpclient.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
+# Modified 2023 by Ryden Graham
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,6 +25,8 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+STANDARD_PORT = 80
+
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
 
@@ -33,21 +36,55 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    def get_path(self, urlData):
+        #Fix path if / is missing
+        path = urlData.path
+        if not path.endswith('/'):
+            path += '/'
+
+        return path
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((host, port))
+        self.socket.connect((host, port or STANDARD_PORT))
+        self.socket.settimeout(30)
         return None
 
-    def get_code(self, data):
+    def get_code(self, headerDict):
+        if 'HTTP/1.1' in headerDict:
+            code = [h for h in headerDict['HTTP/1.1'] if h.isdigit()]
+            if len(code) > 0:
+                return int(code[0])
+        if 'HTTP/1.0' in headerDict:
+            code = [h for h in headerDict['HTTP/1.0'] if h.isdigit()]
+            if len(code) > 0:
+                return int(code[0]) 
         return None
 
     def get_headers(self,data):
-        return None
+        headers = data.split(b'\r\n\r\n')[0]
+        # Michel Keijzers
+        # https://stackoverflow.com/users/1187220/michel-keijzers
+        # Convert list of strings to dictionary
+        # https://stackoverflow.com/a/22981155 
+        headerDictionary = {}
+        for header in headers.split(b'\r\n'):
+            headerInfo = header.decode('utf-8').split(' ')
+            headerDictionary[headerInfo[0]] = headerInfo[1:]
+        return headerDictionary
 
     def get_body(self, data):
-        return None
+        return data.split(b'\r\n\r\n')[1]
+
+    def get_body_encoding(self, headerDict):
+        bodyEncoding = "utf-8"
+        
+        if 'Content-Type:' in headerDict:
+            charset = [h for h in headerDict['Content-Type:'] if h.startswith('charset')]
+            if (len(charset) > 0):
+                bodyEncoding = charset[0][8:]
+
+        return bodyEncoding
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -59,22 +96,79 @@ class HTTPClient(object):
     def recvall(self, sock):
         buffer = bytearray()
         done = False
-        while not done:
-            part = sock.recv(1024)
-            if (part):
-                buffer.extend(part)
-            else:
-                done = not part
-        return buffer.decode('utf-8')
+        try:
+            while not done:
+                part = sock.recv(1024)
+                sock.settimeout(0.1)
+                if (part):
+                    buffer.extend(part)
+                else:
+                    done = not part
+        except socket.timeout:
+            pass
+        sock.settimeout(30)
+        return (self.get_headers(buffer), self.get_body(buffer))
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        # Parse url
+        urlData = urllib.parse.urlparse(url)
+        # Set up socket
+        self.connect(urlData.hostname, urlData.port)
+
+        path = self.get_path(urlData)
+
+        requestBody = ("GET " + path + " HTTP/1.1\r\n" +
+            "Host: " + urlData.hostname + "\r\n" +
+            "Accept-Charset: UTF-8" +
+            "\r\n\r\n")
+
+        # Send GET request
+        self.sendall(requestBody)
+
+        headerDict, body = self.recvall(self.socket)
+
+        bodyEncoding = self.get_body_encoding(headerDict)
+
+        code = self.get_code(headerDict)
+        bodyString = body.decode(bodyEncoding)
+        print(code)
+        if code == 200:
+            print(bodyString)
+        self.close()
+
+        return HTTPResponse(code, bodyString)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        # Parse url
+        urlData = urllib.parse.urlparse(url)
+        # Set up socket
+        self.connect(urlData.hostname, urlData.port)
+
+        postBody = "" if args is None else urllib.parse.urlencode(args)
+
+        path = self.get_path(urlData)
+
+        requestBody = ("POST " + path + " HTTP/1.1\r\n" +
+            "Host: " + urlData.hostname + "\r\n" +
+            "Accept-Charset: UTF-8" + "\r\n" +
+            "Content-Type: application/x-www-form-urlencoded" + "\r\n" +
+            "Content-Length: " + str(len(postBody)) +
+            "\r\n\r\n" +
+            postBody)
+        
+        # Send POST request
+        self.sendall(requestBody)
+
+        headerDict, body = self.recvall(self.socket)
+
+        bodyEncoding = self.get_body_encoding(headerDict)
+
+        code = self.get_code(headerDict)
+        if code == 200:
+            print(body.decode(bodyEncoding))
+        print(code)
+        self.close()
+
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
